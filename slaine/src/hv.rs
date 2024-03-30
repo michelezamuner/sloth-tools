902f77c67594b07334ad4caf1199c54caa15cdd9
Michele Zamuner <michelezamuner@gmail.com>
2024-03-30 13:52:12 +0100

    Remove adapter

--- slaine/src/hv.rs
+++ slaine/src/hv.rs
@@ -1,7 +1,6 @@
-mod adapter;
-
-use crate::vm::Error;
-use adapter::Adapter;
+use crate::vm::{Bus, Device, Error, Input, Rom, Vm};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 #[derive(Debug, PartialEq)]
 pub enum Status {
@@ -10,29 +9,72 @@ pub enum Status {
 }
 
 pub struct Hv {
-  adapter: Adapter,
+  should_stop: Arc<Mutex<bool>>,
+  handle: Option<thread::JoinHandle<Result<(), Error>>>,
+  plug: Option<(u8, u8)>,
 }
 
 impl Hv {
   pub fn new() -> Self {
     Self {
-      adapter: Adapter::new(),
+      should_stop: Arc::new(Mutex::new(false)),
+      handle: None,
+      plug: None,
     }
   }
 
   pub fn status(&mut self) -> Status {
-    self.adapter.status()
+    if self.handle.is_none() {
+      return Status::Off(None);
+    }
+
+    let handle_ref = self.handle.as_ref().unwrap();
+    if !handle_ref.is_finished() {
+      return Status::On;
+    }
+
+    let handle_owned = self.handle.take().unwrap();
+    Status::Off(match handle_owned.join().unwrap() {
+      Ok(_) => None,
+      Err(e) => Some(e),
+    })
   }
 
   pub fn start(&mut self) {
-    self.adapter.start()
+    let input = HvInput::new(Arc::clone(&self.should_stop));
+    let plug = self.plug;
+    self.handle = Some(thread::spawn(move || {
+      let mut bus = Bus::new();
+      if let Some((seg, code)) = plug {
+        let rom: Box<dyn Device> = Box::new(Rom::new(code));
+        bus.register(rom, seg)?;
+      }
+      let vm = Vm::new(bus);
+      vm.run(input)
+    }))
   }
 
   pub fn stop(&mut self) {
-    self.adapter.stop()
+    *self.should_stop.lock().unwrap() = true
   }
 
   pub fn plug(&mut self, seg: u8, code: u8) {
-    self.adapter.plug(seg, code)
+    self.plug = Some((seg, code));
+  }
+}
+
+struct HvInput {
+  should_stop: Arc<Mutex<bool>>,
+}
+
+impl HvInput {
+  fn new(should_stop: Arc<Mutex<bool>>) -> Self {
+    Self { should_stop }
+  }
+}
+
+impl Input for HvInput {
+  fn is_off(&self) -> bool {
+    *self.should_stop.lock().unwrap()
   }
 }
