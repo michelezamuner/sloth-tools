1ad684d019a51acce1d16cee8be6bba7afbacde8
Michele Zamuner <michelezamuner@gmail.com>
2024-12-01 12:04:22 +0100

    Halt machine (#2)

--- sabia/lib/vm.ex
+++ sabia/lib/vm.ex
@@ -3,11 +3,35 @@ defmodule Sabia.Vm do
   Local virtual machine implementation
   """
 
-  def listen do
-    receive do
-      {caller, :state} -> send caller, {:ok, :running}
-    end
+  use Agent
 
-    listen()
+  def create do
+    System.unique_integer()
+  end
+
+  def start(vm) do
+    Agent.start_link(
+      fn -> %{start: :os.system_time(:millisecond), state: :running} end,
+      name: {:global, vm}
+    )
+  end
+
+  def halt(vm) do
+    Agent.update({:global, vm}, fn data -> %{data | state: :halted} end)
+  end
+
+  def state(vm) do
+    Agent.get(
+      {:global, vm},
+      fn
+        %{state: :halted} -> :halted
+        # @todo: fake implementation, we need a process that actually terminates
+        data -> if :os.system_time(:millisecond) - Map.get(data, :start) > 1000 do
+          :halted
+        else
+          :running
+        end
+      end
+    )
   end
 end
--- sabia/test/sabia_test.exs
+++ sabia/test/sabia_test.exs
@@ -3,8 +3,30 @@ defmodule SabiaTest do
 
   test "run vm" do
     vm = Sabia.run()
-    state = Sabia.state(vm)
 
-    assert state == :running
+    assert :running == Sabia.state(vm)
+
+    Sabia.halt(vm)
+
+    assert :halted == Sabia.state(vm)
+  end
+
+  test "run multiple vms" do
+    vm1 = Sabia.run()
+    vm2 = Sabia.run()
+
+    assert :running == Sabia.state(vm1)
+    assert :running == Sabia.state(vm2)
+
+    Sabia.halt(vm1)
+    Sabia.halt(vm2)
+  end
+
+  test "vm terminates on its own" do
+    vm = Sabia.run()
+
+    Process.sleep(2000)
+
+    assert :halted == Sabia.state(vm)
   end
 end
